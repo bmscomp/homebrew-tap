df35ca1c0a53be1075435723e18ab2a9409bd4f3
Said BOUDJELDA <bmscomp@gmail.com>
2026-02-26 22:25:35 +0100

    kates v1.1.0: fix checksums

--- Formula/kates.rb
+++ Formula/kates.rb
@@ -7,20 +7,20 @@ class Kates < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/bmscomp/klster/releases/download/v1.1.0/kates-darwin-arm64.tar.gz"
-      sha256 "59e9f3f1d79eda62abc31b5778e298c505a655084028d24dc6ab93f437b0471f"
+      sha256 "c718a051f1306253b3673761c67480c33e40a6be88ff641e52a887debfb31fb6"
     else
       url "https://github.com/bmscomp/klster/releases/download/v1.1.0/kates-darwin-amd64.tar.gz"
-      sha256 "08affd0f1a2eb1c20096cf129e6b61e742864086379864ae1f981dbbb36fb323"
+      sha256 "c082b3a6b801f0e05c0967357fc0c0bfcefaeb57f73565752265d20c5afc4a8d"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/bmscomp/klster/releases/download/v1.1.0/kates-linux-arm64.tar.gz"
-      sha256 "323fb12e3564064afccde86c73bcbfc27caefd2604a4c46662d190e9f7406804"
+      sha256 "4be39b41c9f5c2eb6e040910daf2092d3df3f3cb1bcb856d02c1fbaa8708d38d"
     else
       url "https://github.com/bmscomp/klster/releases/download/v1.1.0/kates-linux-amd64.tar.gz"
-      sha256 "c23a36953a2a727c568274237eae1d2182f7e898fd987462e67b852d330087ee"
+      sha256 "26735cf7bc596687d64cf0be9751ad70bf34a1751bc7dc9be785cdcdf59f29cf"
     end
   end
 
